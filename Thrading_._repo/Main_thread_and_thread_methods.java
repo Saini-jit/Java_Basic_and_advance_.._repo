bd170396f02b180a3882d2b1982c15d072dd70ff
Saini-jit <sainijitendra75700@gmail.com>
2026-08-26 14:31:18 +0530

    Some methods of thread class are added

--- Thrading_._repo/Main_thread_and_thread_methods.java
+++ Thrading_._repo/Main_thread_and_thread_methods.java
@@ -6,10 +6,32 @@ The default name of the main thread is "main".
 The default priority of the main thread is 5.
 It usually finishes last as it may perform cleanup and shutdown tasks.*/
 // to control the main thread we have to create reference which is created by Thread.currentThread()
+
+int num;
+Main_thread_and_thread_methods(int num){
+    this.num=num;
+}
+
+public void run(){
+    System.out.println(num);
+}
     public static void main(String[] args) {
+        Thread tc=new Main_thread_and_thread_methods(5);
+        tc.start();;
         Thread th= Thread.currentThread();
         System.out.println(th.getName());
         th.setName("Newthread");
         System.out.println(th.getName());
+        System.out.println(th.getPriority());
+        th.setPriority(5);
+        System.out.println(th.getPriority());
+        //Child
+        System.out.println(tc.getName());
+        tc.setName("Newthread");
+        System.out.println(tc.getName());
+        System.out.println(tc.getPriority());
+        tc.setPriority(5);
+        System.out.println(tc.getPriority());
+        
     }
 }
